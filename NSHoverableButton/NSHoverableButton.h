9ec06d1ca57155485c5c2c9c72b90abf1c69d1d9
Jiawei Li <c@rallets.com>
2018-05-25 09:33:10 +0800

    Initial Commit

---
+++ NSHoverableButton/NSHoverableButton.h
@@ -0,0 +1,19 @@
+//
+//  NSHoverableButton.h
+//  NSHoverableButton
+//
+//  Created by Jiawei Li on 25/05/2018.
+//  Copyright © 2018 Jiawei Li. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NSHoverableButton.
+FOUNDATION_EXPORT double NSHoverableButtonVersionNumber;
+
+//! Project version string for NSHoverableButton.
+FOUNDATION_EXPORT const unsigned char NSHoverableButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSHoverableButton/PublicHeader.h>
+
+
