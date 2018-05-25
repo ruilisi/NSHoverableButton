04c150d9134aebaff61405fbe320842a8c3e2675
Jiawei Li <c@rallets.com>
2018-05-25 16:42:45 +0800

    init

---
+++ NSHoverableButton.swift
@@ -0,0 +1,42 @@
+//
+//  NSHoverableButton.swift
+//  NSHoverableButton
+//
+//  Created by Jiawei Li on 25/05/2018.
+//  Copyright © 2018 Paiyou. All rights reserved.
+//
+import Foundation
+import Cocoa
+
+open class NSHoverableButton: NSButton {
+    @IBInspectable public var hoveringImage: NSImage?
+    @IBInspectable public var unhoveringImage: NSImage?
+    
+    override open func awakeFromNib() {
+        super.awakeFromNib()
+        let area = NSTrackingArea.init(rect: self.bounds,
+                                       options: [.mouseEnteredAndExited, .inVisibleRect, .assumeInside, .activeAlways],
+                                       owner: self,
+                                       userInfo: nil)
+        self.addTrackingArea(area)
+        self.image = unhoveringImage ?? self.image ?? NSImage.init()
+    }
+    
+    override open func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        self.addCursorRect(self.bounds, cursor: NSCursor.pointingHand)
+    }
+    override open func mouseEntered(with event: NSEvent) {
+        guard self.image == hoveringImage ?? self.image else {
+            self.image = hoveringImage ?? self.image
+            return
+        }
+    }
+    
+    override open func mouseExited(with event: NSEvent) {
+        guard self.image == unhoveringImage ?? self.image else {
+            self.image = unhoveringImage ?? self.image
+            return
+        }
+    }
+}
---
+++ NSHoverableButtonDemo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NSHoverableButtonDemo
+//
+//  Created by Jiawei Li on 25/05/2018.
+//  Copyright © 2018 Jiawei Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
